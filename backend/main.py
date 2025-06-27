5ebdddd8609f9ba87121033b15d57c11673841a2
khushi <khushihp7903@gmail.com>
2025-06-27 15:25:05 +0530

    update main.py file

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import prediction
+from app.routes import get_price as price_router
 from app.routes import symbols  # Import your new route module
 
 app = FastAPI(title="Stock Price Prediction API")
@@ -8,6 +9,7 @@ app = FastAPI(title="Stock Price Prediction API")
 # Register prediction routes
 app.include_router(prediction.router, prefix="/api")
 app.include_router(symbols.router, prefix="/api")
+app.include_router(price_router.router, prefix="/api")
 
 app.add_middleware(
     CORSMiddleware,
